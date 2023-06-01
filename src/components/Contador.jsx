c26b325f19731036a5ccab07d170acc8bdb7a7c1
PedroAbregoOlivares <pedroabregoolivares96@gmail.com>
2023-05-31 19:25:05 -0600

    commit

---
+++ src/components/Contador.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import "./contador.css";
+
+const Contador = () => {
+  const [contador, setContador] = useState(0);
+
+  const incrementarContador = () => {
+    if (contador >= 0) {
+      setContador(contador + 1);
+    }
+  };
+
+  const reset = () => {
+    setContador( 0 )
+  }
+
+  const decrementarContador = () => {
+    if (contador >= 1) {
+      setContador(contador - 1);
+    }
+  };
+
+  return (
+    <>
+      <div className="center">
+        <div class="main"></div>
+        <div className="contador">
+          <h1>{contador}</h1>
+        </div>
+        <div className="boton">
+          <button className="btn" onClick={decrementarContador}>
+            <i class="fas fa-minus"></i>
+          </button>
+          <button className="btn" onClick={reset}>Reset</button>
+          <button className="btn" onClick={incrementarContador}>
+            <i class="fas fa-plus"></i>
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Contador;
